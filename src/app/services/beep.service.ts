0ca24f821744c82b4d300a505b5e7903925a32e6
Stefan Braune <stefan.braune@gmail.com>
2017-10-01 19:50:39 +0200

    feat(): support for html5 audio

--- src/app/services/beep.service.ts
+++ src/app/services/beep.service.ts
@@ -1,33 +1,68 @@
 import { Injectable } from '@angular/core';
-
+import { Platform } from 'ionic-angular';
 import { Vibration } from '@ionic-native/vibration';
 import { NativeAudio } from '@ionic-native/native-audio';
 
 @Injectable()
 export class BeepService {
+  private audioType: string = 'html5';
+  private sounds: any = [];
+
   public constructor(
+    private platform: Platform,
     private vibration: Vibration,
-    private audio: NativeAudio
+    private nativeAudio: NativeAudio
   ) {
-    audio.preloadSimple('short', 'assets/sounds/sin_440_300.wav').then((result) => {
-      console.info('Successfully loaded short beep sound:', result);
-    }).catch((error) => {
-      console.warn('Error while loading short beep sound:', error);
-    });
-    audio.preloadSimple('long', 'assets/sounds/sin_880_1500.wav').then((result) => {
-      console.info('Successfully loaded long beep sound:', result);
-    }).catch((error) => {
-      console.warn('Error while loading long beep sound:', error);
-    });
+    if (platform.is('cordova')) {
+      this.audioType = 'native';
+    }
+
+    this.preload('short', 'assets/sounds/sin_440_300.wav');
+    this.preload('long', 'assets/sounds/sin_880_1500.wav');
   }
-  
+
   public beepShort() {
-    this.audio.play('short');
+    this.play('short');
     this.vibration.vibrate(300);
   }
-  
+
   public beepLong() {
-    this.audio.play('long');
+    this.play('long');
     this.vibration.vibrate(1500);
+  }
+
+  public preload(key: string, asset: string) {
+    if (this.audioType === 'html5') {
+      this.sounds.push({
+        key: key,
+        asset: asset,
+        type: 'html5'
+      });
+    } else {
+      this.nativeAudio.preloadSimple(key, asset).then((result) => {
+        console.info('Successfully loaded sound:', key, asset, result);
+      }).catch((error) => {
+        console.warn('Error while loading sound:', key, asset, error);
+      });
+      this.sounds.push({
+        key: key,
+        asset: key,
+        type: 'native'
+      });
+    }
+  }
+
+  public play(key: string) {
+    let audio = this.sounds.find((sound) => sound.key === key);
+    if (audio.type === 'html5') {
+      let audioAsset = new Audio(audio.asset);
+      audioAsset.play();
+    } else {
+      this.nativeAudio.play(audio.asset).then((res) => {
+        console.log(res);
+      }, (err) => {
+        console.warn(err);
+      });
+    }
   }
 }
